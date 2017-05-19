e5286669985c8abcce91289fc2c3444b2c897e0d
biprateep <biprateep@gmail.com>
2017-05-19 23:47:31 +0000

    Finished the Homework

--- MySineWave.py
+++ MySineWave.py
@@ -1,2 +1,13 @@
 import numpy as np
-import matplot.pyplot as plt
+import matplotlib.pyplot as plt
+
+def MySineWave(waveLength):
+    step=0.01
+    xData=np.arange(-2*np.pi, 2**np.pi+step, step)
+    yData=np.sin(2*np.pi*xData/float(waveLength))
+    plt.plot(xData,yData)
+    plt.grid()
+    plt.xlim(-2*np.pi,2**np.pi)
+    plt.show()
+    
+MySineWave(2.0)
